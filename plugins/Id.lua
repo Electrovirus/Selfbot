2439c5c4173793e8bcaf207d62d9e272272196ed
Electrovirus <E.electrovirus@gmail.com>
2016-09-02 20:18:57 +0430

    Selfbot V.1

--- plugins/Id.lua
+++ plugins/Id.lua
@@ -1,6 +1,6 @@
 do
 function run(msg, matches)
-  return "➖➖➖➖➖➖➖➖➖➖➖🌎\nGROUP ID : 100"..msg.to.id.."\n👥GROUP NAME : "..msg.to.title.."\n➖➖➖➖➖➖➖➖➖➖➖\n🗣NAME : "..(msg.from.first_name or '-').."\n🌟`USERNAME` : @"..(msg.from.username or '-').."\n🆔Tg ID : "..msg.from.id.."\n➖➖➖➖➖➖➖➖➖➖➖\n📱PHONENUMBER : +"..(msg.from.phone or 'Error 404').."\n➖➖➖➖➖➖➖➖➖➖➖\n💬YOUR LINK : telegram.me/"..(msg.from.username or '-').."\n"
+  return "➖➖➖➖➖➖➖➖➖➖➖\n🌎GROUP ID : 100"..msg.to.id.."\n👥GROUP NAME : "..msg.to.title.."\n➖➖➖➖➖➖➖➖➖➖➖\n🗣NAME : "..(msg.from.first_name or '-').."\n🌟`USERNAME` : @"..(msg.from.username or '-').."\n🆔Tg ID : "..msg.from.id.."\n➖➖➖➖➖➖➖➖➖➖➖\n📱PHONENUMBER : +"..(msg.from.phone or 'Error 404').."\n➖➖➖➖➖➖➖➖➖➖➖\n💬YOUR LINK : telegram.me/"..(msg.from.username or '-').."\n"
 end
 return {
   description = "info", 
