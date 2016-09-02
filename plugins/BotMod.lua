bee8fd776bfbd49110440a3af9643d1bee7aaa33
Electrovirus <E.electrovirus@gmail.com>
2016-09-02 20:12:32 +0430

    Selfbot V.1

--- plugins/BotMod.lua
+++ plugins/BotMod.lua
@@ -16,13 +16,13 @@ local function enable_channel(receiver)
 	end
 
 	if _config.disabled_channels[receiver] == nil then
-		return "Bot Is Not Off :)"
+		return "Bot is already online ▶"
 	end
 	
 	_config.disabled_channels[receiver] = false
 
 	save_config()
-	return "Bot Is On Now :D"
+	return "Bot is online ▶"
 end
 
 local function disable_channel( receiver )
@@ -33,7 +33,7 @@ local function disable_channel( receiver )
 	_config.disabled_channels[receiver] = true
 
 	save_config()
-	return "Bot Is Off Now :/"
+	return "Bot is offline ■"
 end
 
 local function pre_process(msg)
