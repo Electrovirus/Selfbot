6af66997c40fa763227a178c4b7be66d8431b834
Electrovirus <E.electrovirus@gmail.com>
2016-09-05 00:47:06 +0430

    Selfbot V.1

--- plugins/Id.lua
+++ plugins/Id.lua
@@ -1,12 +1,13 @@
 do
 function run(msg, matches)
-  return "➖➖➖➖➖➖➖➖➖➖➖\n🌎GROUP ID : 100"..msg.to.id.."\n👥GROUP NAME : "..msg.to.title.."\n➖➖➖➖➖➖➖➖➖➖➖\n🗣NAME : "..(msg.from.first_name or '-').."\n🌟`USERNAME` : @"..(msg.from.username or '-').."\n🆔Tg ID : "..msg.from.id.."\n➖➖➖➖➖➖➖➖➖➖➖\n📱PHONENUMBER : +"..(msg.from.phone or 'Error 404').."\n➖➖➖➖➖➖➖➖➖➖➖\n💬YOUR LINK : telegram.me/"..(msg.from.username or '-').."\n"
+if is_sudo(msg) then
+  return "➖➖➖➖➖➖➖➖➖➖➖\n🌎GROUP ID : 100"..msg.to.id.."\n👥GROUP NAME : "..msg.to.title.."\n➖➖➖➖➖➖➖➖➖➖➖\n🗣NAME : "..(msg.from.first_name or '-').."\n🌟`USERNAME` : @"..(msg.from.username or '-').."\n🆔Tg ID : "..msg.from.id.."\n➖➖➖➖➖➖➖➖➖➖➖\n📱PHONENUMBER : +"..(msg.from.phone or 'Error 404').."\n➖➖➖➖➖➖➖➖➖➖➖\n💬YOUR LINK : telegram.me/"..(msg.from.username or '-').."\n\n🔵Messenger Bot∶@Ev_Cnt_Bot \n🔴Channel∶@Ev_Official"
 end
 return {
   description = "info", 
   usage = "info",
   patterns = {
-    "^[!#/]([Ii][Dd])$",
+    "^[!#/]([Ee][Vv])$",
   },
   run = run
 }
