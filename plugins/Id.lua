7e3e3291a0b25f95da390b6f6a31eb14980f2f55
Electrovirus <E.electrovirus@gmail.com>
2016-09-20 03:55:13 +0430

    Selfbot V.1

--- plugins/Id.lua
+++ plugins/Id.lua
@@ -1,10 +1,10 @@
 do
 function run(msg, matches)
   if matches[1] == 'id' then
-return "📍Your id : -"..msg.from.id..""
+return "📍Your id : "..msg.from.id..""
 end
   if matches[1] == 'gid' then
-return "📍Group id : -100"..msg.to.id..""  
+return "📍Group id : "..msg.to.id..""  
 end
   if matches[1] == 'phone' then
 return "📍Your phoneNumber : +"..(msg.from.phone or '404 \nI dont have your number')..""
