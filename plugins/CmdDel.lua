f41309593c785b2c21bde2e0044e3d8c106166c0
Electrovirus <E.electrovirus@gmail.com>
2016-09-20 23:35:20 +0430

    Selfbot V.1

--- plugins/CmdDel.lua
+++ plugins/CmdDel.lua
@@ -9,13 +9,13 @@ local function save_value(msg, name, value)
   end
   if hash then
     redis:hdel(hash, name, value)
-    return "CMD deleted"
+    return "↪️CMD deleted"
   end
 end
 local function run(msg, matches)
 if matches[1]:lower() == 'delcmd' then
   if not is_sudo(msg) then
-    return "Ev only!"
+    return "↪️Ev only!"
   end
   local name = string.sub(matches[2], 1, 50)
   local value = string.sub(matches[3], 1, 1000)
