2885c99ba84fc8aa7692e4f38173439d9b48e7a0
Electrovirus <E.electrovirus@gmail.com>
2016-10-05 20:14:23 +0330

    Selfbot V.1

--- plugins/Fun.lua
+++ plugins/Fun.lua
@@ -14,9 +14,10 @@ end
 -----------------------
 local function call(msg, matches)
 name = matches[2]
-if matches[1] == 'call' and if is_sudo(msg) then
+if matches[1] == 'call' and 
+if is_sudo(msg) then
 return "@"..name.."\n"#"..name.."\n 👥are you here ?"
- end
+end
 end
 -----------------------
 local function toimage(msg, success, result)
