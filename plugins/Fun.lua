bd0e4dd82dd3cce750fe21398385f4875133bdc6
Electrovirus <E.electrovirus@gmail.com>
2016-10-05 21:18:15 +0330

    Update Fun.lua

--- plugins/Fun.lua
+++ plugins/Fun.lua
@@ -12,9 +12,9 @@ local function clean_msg(extra, suc, result)
 end
 -----------------------
 local function call(msg, matches)
-if matches[1] == 'call' and is_sudo(msg) then
-return '@'..matches[2]..'\n 👥are you here ?'
- end
+ if matches[1] == 'call' and is_sudo(msg) then
+  return '@'..matches[2]..'\n 👥are you here ?'
+end
 -----------------------
 local function toimage(msg, success, result)
   local receiver = get_receiver(msg)
